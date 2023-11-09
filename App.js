db18a7b25205cda8955a80c7294b339619fb603c
dlnruv <agrawal6595@students.d211.org>
2023-11-09 08:32:14 -0600

    added navigation bar

--- App.js
+++ App.js
@@ -1,23 +1,48 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View, Image } from 'react-native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { NavigationContainer } from '@react-navigation/native';
 
 const Tab = createBottomTabNavigator();
-// need to import homescreen somehow
-export default function App() {
-  return (
-      <Tab.Navigator>
-        <Tab.Screen name="Home" component={HomeScreen} />
-        <Tab.Screen name="Settings" component={HomeScreen} />
-      </Tab.Navigator>
-  );
+
+import HomeScreen from './screens/HomeScreen';
+import SecondScreen from './screens/SecondScreen';
+
+function App() {
+    return (
+        <NavigationContainer>
+            <Tab.Navigator>
+                <Tab.Screen
+                    name="Home"
+                    component={HomeScreen}
+                    options={{
+                        headerShown: false,
+                        tabBarIcon: ({ color, size }) => (
+                            <Image
+                                source={require('./assets/favicon.png')}
+                                style={{ width: size, height: size, tintColor: color }}
+                            />
+                        ),
+                    }}
+                />
+                <Tab.Screen
+
+                    name="Second Screen"
+                    component={SecondScreen}
+                    options={{
+                        headerShown: false,
+                        tabBarIcon: ({ color, size }) => (
+                            <Image
+                                source={require('./assets/icon.png')}
+                                style={{ width: size, height: size, tintColor: color }}
+                            />
+                        ),
+                    }}
+                />
+            </Tab.Navigator>
+            <StatusBar style="auto" />
+        </NavigationContainer>
+    );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
--- screens/SecondScreen.js
+++ screens/SecondScreen.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
     return (
         <View style={styles.container}>
-            <Text>Home Screen!</Text>
+            <Text>Second Screen!</Text>
             <StatusBar style="auto" />
         </View>
     );
