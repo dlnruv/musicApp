be92e3abd4260bcd403b1d52b815316c698d760b
dlnruv <agrawal6595@students.d211.org>
2023-11-14 09:05:02 -0600

    trying to fix vinyl bg / no getColor function in FastImage

--- screens/TrackItem.js
+++ screens/TrackItem.js
@@ -1,30 +1,66 @@
 // TrackItem.js
-import React from 'react';
-import {View, Text, Image, StyleSheet} from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { View, Text, StyleSheet, Animated } from 'react-native';
+import FastImage from 'react-native-fast-image';
 
 const TrackItem = ({ track, startPlayback }) => {
+    const [spinValue] = useState(new Animated.Value(0));
+    const [backgroundColor, setBackgroundColor] = useState('transparent');
+
+    useEffect(() => {
+        const fetchImageColor = async () => {
+            try {
+                const color = await FastImage.getColor(track.album.images[0]?.url);
+                setBackgroundColor(color);
+            } catch (error) {
+                console.error('Error fetching image color:', error);
+            }
+        };
+
+        fetchImageColor();
+
+        // Start the rotation animation when the component mounts
+        Animated.loop(
+            Animated.timing(spinValue, {
+                toValue: 1,
+                duration: 3000,
+                useNativeDriver: true,
+            })
+        ).start();
+    }, [spinValue, track.album.images]);
+
+    const spin = spinValue.interpolate({
+        inputRange: [0, 1],
+        outputRange: ['0deg', '360deg'],
+    });
+
     return (
         <View
-            style={styles.trackContainer}
+            style={[styles.trackContainer, { backgroundColor }]}
             onStartShouldSetResponder={() => {
                 startPlayback(track.uri);
             }}
         >
-            <Image style={styles.trackImage} source={{ uri: track.album.images[0]?.url }} />
+            <Animated.Image
+                style={[styles.trackImage, { transform: [{ rotate: spin }] }]}
+                source={{ uri: track.album.images[0]?.url }}
+                resizeMode={FastImage.resizeMode.cover}
+            />
             <Text style={styles.trackName}>{track.name}</Text>
             <Text style={styles.artistName}>{track.artists[0].name}</Text>
         </View>
     );
 };
+
 const styles = StyleSheet.create({
     trackContainer: {
         marginBottom: 20,
         alignItems: 'center',
     },
     trackImage: {
-        width: '100%',
-        height: 600,
-        borderRadius: 10,
+        width: 200,
+        height: 200,
+        borderRadius: 100,
         marginBottom: 8,
     },
     trackName: {
@@ -36,7 +72,7 @@ const styles = StyleSheet.create({
         fontSize: 14,
         color: 'gray',
         marginBottom: 8,
-    }
+    },
 });
 
 export default TrackItem;
