66d19074b3dc9b46c65bc072336ae9548cb9ec0c
divij <mithsagar9453@students.d211.org>
2023-11-20 08:46:39 -0600

    e

--- screens/RightScreen.js
+++ screens/RightScreen.js
@@ -1,20 +1,51 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect, useRef, useState } from "react";
+import {
+    query,
+    collection,
+    orderBy,
+    onSnapshot,
+    limit,
+} from "firebase/firestore";
+import { db } from "../firebase";
+import Message from "./Message";
+import SendMessage from "./SendMessage";
 
-export default function App() {
+const ChatBox = () => {
+    const [messages, setMessages] = useState([]);
+    const scroll = useRef();
+
+    useEffect(() => {
+        const q = query(
+            collection(db, "messages"),
+            orderBy("createdAt", "desc"),
+            limit(50)
+        );
+
+        const unsubscribe = onSnapshot(q, (QuerySnapshot) => {
+            const fetchedMessages = [];
+            QuerySnapshot.forEach((doc) => {
+                fetchedMessages.push({ ...doc.data(), id: doc.id });
+            });
+            const sortedMessages = fetchedMessages.sort(
+                (a, b) => a.createdAt - b.createdAt
+            );
+            setMessages(sortedMessages);
+        });
+        return () => unsubscribe;
+    }, []);
+
     return (
-        <View style={styles.container}>
-            <Text>Right Screen!</Text>
-            <StatusBar style="auto" />
-        </View>
+        <main className="chat-box">
+            <div className="messages-wrapper">
+                {messages?.map((message) => (
+                    <Message key={message.id} message={message} />
+                ))}
+            </div>
+            {/* when a new message enters the chat, the screen scrolls down to the scroll div */}
+            <span ref={scroll}></span>
+            <SendMessage scroll={scroll} />
+        </main>
     );
-}
+};
 
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#fff',
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
-});
+export default ChatBox;
